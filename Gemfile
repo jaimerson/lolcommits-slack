22838cf2ae7806a8b837e772e96b4f37d28c8e9f
Jaimerson Araujo <jaimersonaraujo@gmail.com>
2014-02-28 16:20:18 -0300

    Add file uploads

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@ source 'https://rubygems.org'
 
 ruby '2.1.0'
 
-gem 'httparty'
+gem 'httmultiparty'
 gem 'listen', '~> 2.0'
 gem 'dotenv'
--- listener.rb
+++ listener.rb
@@ -1,14 +1,21 @@
 require 'listen'
-require 'httparty'
+require 'httmultiparty'
 require 'dotenv'
 
 Dotenv.load
 path = ENV['LOLCOMMITS_REPO_IMAGES']
+token = ENV['SLACK_TOKEN']
 
-listener = Listen.to(path) do|modified, added|
-  puts "#{added}"
+listener = Listen.to(path) do |modified, added|
+  file = added.first
+  if file
+    options = { file: File.new(file) }
+    HTTMultiParty.post("https://slack.com/api/files.upload?token=#{token}",
+                       body: options)
+    puts "Uploading #{file} to slack..."
+  end
 end
 
-puts "Started listening on #{path}."
+puts "Started listening on #{path}"
 listener.start
 sleep
